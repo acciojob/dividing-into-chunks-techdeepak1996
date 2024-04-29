63556852aaa8a8a84b8fb017d3fed72db6bdcdf2
Tech Deepak <141490582+techdeepak1996@users.noreply.github.com>
2024-04-29 17:37:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
-
 const divide = (arr, n) => {
-  // Write your code here
+  const result = [];
+  let currentSubarray = [];
+
+  for (let num of arr) {
+    if (currentSubarray.reduce((acc, val) => acc + val, 0) + num <= n) {
+      currentSubarray.push(num);
+    } else {
+      result.push(currentSubarray);
+      currentSubarray = [num];
+    }
+  }
+
+  if (currentSubarray.length > 0) {
+    result.push(currentSubarray);
+  }
+
+  return result;
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+const n = 5;
+console.log(JSON.stringify(divide(arr, n)));
